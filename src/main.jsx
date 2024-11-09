e7bfb747c01a8992493b22de7083646d2e203f23
Matheus Souza <1002matheussouza@gmail.com>
2024-11-09 17:31:02 -0300

    Initial Commit

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./pages/App";
+import TaskPage from "./pages/TaskPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/task",
+    element: <TaskPage/>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+);
